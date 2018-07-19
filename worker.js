9fa165f6d0d231d6fa3d16dd791d0e8780d924c2
Christian Vaughn <cvaughn55@gmail.com>
2018-07-19 10:05:33 -0700

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,52 +1,36 @@
-var PRECACHE = 'precache-v1';
-var RUNTIME = 'runtime';
+// use a cacheName for cache versioning
+var cacheName = 'v1:static';
 
-// list the files you want cached by the service worker
-PRECACHE_URLS = [
-  'index.html',
-  './',
-  'style.css',
-  'main.js'
-];
-
-
-// the rest below handles the installing and caching
-self.addEventListener('install', event => {
-  event.waitUntil(
-     caches.open(PRECACHE).then(cache => cache.addAll(PRECACHE_URLS)).then(self.skipWaiting())
-  );
-});
-
-self.addEventListener('activate', event => {
-  const currentCaches = [PRECACHE, RUNTIME];
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return cacheNames.filter(cacheName => !currentCaches.includes(cacheName));
-    }).then(cachesToDelete => {
-      return Promise.all(cachesToDelete.map(cacheToDelete => {
-        return caches.delete(cacheToDelete);
-      }));
-    }).then(() => self.clients.claim())
-  );
-});
-
-self.addEventListener('fetch', event => {
-  if (event.request.url.startsWith(self.location.origin)) {
-    event.respondWith(
-      caches.match(event.request).then(cachedResponse => {
-        if (cachedResponse) {
-          return cachedResponse;
-        }
-
-        return caches.open(RUNTIME).then(cache => {
-          return fetch(event.request).then(response => {
-            // Put a copy of the response in the runtime cache.
-            return cache.put(event.request, response.clone()).then(() => {
-              return response;
+// during the install phase you usually want to cache static assets
+self.addEventListener('install', function(e) {
+    // once the SW is installed, go ahead and fetch the resources to make this work offline
+    e.waitUntil(
+        caches.open(cacheName).then(function(cache) {
+            return cache.addAll([
+                './',
+                './css/style.css',
+                './js/build/script.min.js',
+                './js/build/vendor.min.js',
+                './css/fonts/roboto.woff',
+                './offline.html'
+            ]).then(function() {
+                self.skipWaiting();
             });
-          });
-        });
-      })
+        })
     );
-  }
+});
+
+// when the browser fetches a url
+self.addEventListener('fetch', function(event) {
+    // either respond with the cached object or go ahead and fetch the actual url
+    event.respondWith(
+        caches.match(event.request).then(function(response) {
+            if (response) {
+                // retrieve from cache
+                return response;
+            }
+            // fetch as normal
+            return fetch(event.request);
+        })
+    );
 });
