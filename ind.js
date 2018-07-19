0a2a8e7c25fff85884100eddbbfd64ad1008b9fe
Christian Vaughn <cvaughn55@gmail.com>
2018-07-19 10:06:44 -0700

    Update ind.js

--- ind.js
+++ ind.js
@@ -1,12 +1,7 @@
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('worker.js');
+navigator.serviceWorker.register('./worker.js').then(function(reg) {
+    console.log('Successfully registered service worker', reg);
+}).catch(function(err) {
+    console.warn('Error whilst registering service worker', err);
+});
 }
-
-// link to a image file
-var iconUrl = 'https://www.seeklogo.net/wp-content/uploads/2014/10/Google-Chrome-logo-vector-download.png';
-
-// create the <img> html element
-// on first load it will request the image
-// second time it will load it from cache directly thanks to the service worker
-var imgElement = document.createElement('img');
-imgElement.src = iconUrl;
