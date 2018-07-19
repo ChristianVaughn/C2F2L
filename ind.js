051fcae881833dd137842d1fcc6524fa0466ddd4
Christian Vaughn <cvaughn55@gmail.com>
2018-07-19 10:31:13 -0700

    Delete ind.js

--- ind.js
+++
@@ -1,7 +0,0 @@
-if ('serviceWorker' in navigator) {
-navigator.serviceWorker.register('./worker.js').then(function(reg) {
-    console.log('Successfully registered service worker', reg);
-}).catch(function(err) {
-    console.warn('Error whilst registering service worker', err);
-});
-}
